98bbb674370fa4ae05bafc0161030db6a2329e00
ArthurDenis62 <arthurdenis.pro@gmail.com>
2024-02-01 11:38:11 +0100

    gallery grid

--- scripts/templates/image.js
+++ scripts/templates/image.js
@@ -2,7 +2,7 @@ function displayImageGalery (media, photographer) {
     const imageElement = document.createElement('div');
     imageElement.classList.add('photograph');
     imageElement.innerHTML = `<a href="assets/images/SamplePhotos/${photographer.name.split(' ')[0]}/${media.image}">
-                                  <img src="assets/images/SamplePhotos/${photographer.name.split(' ')[0]}/${media.image}" alt="${media.title}" class="photographImg" />
+                                  <img src="assets/images/SamplePhotos/${photographer.name.split(' ')[0]}/${media.image}" alt="${media.title}" class="photographImg" alt="${media.title}" />
                                   <div class="details">
                                       <h3>${media.title}</h3>
                                       <p class="likesText" tabindex="0"><span class="numberLikes">${media.likes}</span> <img src="assets/images/coeur.svg" alt="coeur" /></p>
